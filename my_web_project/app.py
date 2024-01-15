9a75068b4b6051b2fd13c67834bf7cccf53984bc
swapnil.chatterjee <swapnil.chatterjee@accenture.com>
2024-01-15 17:24:49 +0530

    Get rule from ttp id

--- my_web_project/app.py
+++ my_web_project/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-import requests
-from config import API_KEY
+import requests, os, glob, yaml, urllib.parse
+from config import API_KEY, DIRECTORY_PATH
 app = Flask(__name__)
 
 url = "https://www.virustotal.com"
@@ -110,10 +110,69 @@ def threat_actor_ttps(actor_id, actor_name):
     return render_template('TA_TTP.html', all_ttps=all_ttps, actor_name=actor_name)
 
 
-# Subpage 3
-@app.route('/subpage3')
-def subpage3():
-    return render_template('page3.html')
+# Search rule by ID
+@app.route('/search_by_id/<tech_id>')
+def search_by_id(tech_id):
+    directory_path = DIRECTORY_PATH
+    file_type = "yml"
+    search_string = tech_id  # Replace with the desired ID tags
+    matching_files = []
+    
+    # Use os.walk to traverse through all directories and subdirectories
+    for directory, _, _ in os.walk(directory_path):
+
+        # Create a pattern to match files with the specified file type
+        pattern = os.path.join(directory, f'*.{file_type}')
+
+        # Use glob to find files matching the pattern
+        for file_path in glob.glob(pattern):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                try:
+                    # yaml_content = yaml.safe_load_all(file)
+                    
+                    # Use yaml.safe_load_all() to load multiple documents
+                    for yaml_content in yaml.safe_load_all(file):
+                        if isinstance(yaml_content, dict):
+                            
+                            #Option 1
+                            # Check if the search string is present in the 'tags' field
+                            # tags = yaml_content.get('tags', [])
+                            # if any(search_string in tag for tag in tags):
+                            #     matching_files.append(file_path)
+                            
+                            #OR---- Option 2
+                                
+                            # Flatten the YAML content into a list of strings for searching
+                            yaml_strings = [str(value) for key, value in yaml_content.items()]
+                            # Check if the search_string is present in any of the strings
+                            if any(search_string.lower() in yaml_str.lower() for yaml_str in yaml_strings):
+                                matching_files.append(file_path)
+                except yaml.YAMLError as e:
+                    print(f"Error reading YAML in file {file_path}: {e}")
+        
+    if matching_files:
+        count_of_members = len(matching_files)
+        print(f"Number of matching files: {count_of_members}")
+        print("Matching files:")
+        for file_path in matching_files:
+            print(file_path)
+        
+    else:
+        print("No matching files found.")
+        
+    return render_template('GET_YML.html', matching_files= matching_files, tech_id= tech_id)
+
+@app.route('/rules_viewer/<path:filename>')
+def rules_viewer(filename):
+    try:
+        with open(filename, 'r') as file:
+            rule = file.read()
+        return render_template('RULES_VIEW.html', filename=filename, rule=rule)
+    except Exception as e:
+        print("Exception:", str(e))
+        # Redirect to GET request to avoid resubmission on page refresh
+        return render_template('RULES_VIEW.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
